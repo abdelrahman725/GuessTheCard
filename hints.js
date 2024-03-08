d27568bbe02d15af7a9c107f4df5c42f9d18c423
bedo <bedokhaled66@gmail.com>
2024-03-08 23:48:52 +0200

    Add 5 Hints in english

--- hints.js
+++ hints.js
@@ -7,13 +7,19 @@ const CardsHints = {
     en: `How many rules say "do not talk about Fight Club"`,
     ar: "أريد فدانين من الأرض لزراعتها",
   },
-  "3_clubs": { en: "The player scored Hat Trick for his ...", ar: "----" },
+  "3_clubs": {
+    en: "The player scored Hat Trick for his ...?",
+    ar: "----",
+  },
   "4_clubs": { en: "Can you draw the body limbs on a tree leaf ?", ar: "----" },
   "5_clubs": {
     en: "The starfish joined the fight with Edward Norton and Brad Pitt",
     ar: "----",
   },
-  "6_clubs": { en: "I threw the dice hopping for luck !", ar: "----" },
+  "6_clubs": {
+    en: "I threw the dice hoping for luck !",
+    ar: "----",
+  },
   "7_clubs": {
     en: "No one with the deadly sins can enter that group/organization",
     ar: "----",
@@ -33,13 +39,17 @@ const CardsHints = {
     en: "The workers need half-dozen of digging tools",
     ar: "----",
   },
-  // days of the week
-  "7_spades": { en: "d", ar: "----" },
-  // use octagon or spider
-  "8_spades": { en: "----", ar: "----" },
+  "7_spades": {
+    en: "A week long of black cards which are not leaves",
+    ar: "----",
+  },
+  "8_spades": {
+    en: "The spider turned the black-painted hearts upside down",
+    ar: "----",
+  },
   "9_spades": { en: "I seee a soldier playing Sudoku", ar: "----" },
   "10_spades": { en: "The army spent a decade fighting", ar: "----" },
-  "1_diamonds": { en: "----", ar: "----" },
+  "1_diamonds": { en: "During mining it was the first", ar: "----" },
   "2_diamonds": { en: "----", ar: "التاجر" },
   "3_diamonds": {
     en: "This Trilogy is expensive, brilliant and hard",
@@ -56,17 +66,22 @@ const CardsHints = {
   "6_diamonds": { en: "The Treasure chest is hexagon-shaped", ar: "----" },
   "7_diamonds": {
     en: "I sparkle and refracts light into a rainbow of colors",
-    ar: "",
+    ar: "----",
   },
   "8_diamonds": { en: "I keep my Jewelry in a sand clock", ar: "----" },
   "9_diamonds": {
     en: "A number of high-cost rings were gifted to the race of men",
     ar: "التاجرة حامل في طفل",
   },
-  "10_diamonds": { en: "----", ar: "----" },
+  "10_diamonds": {
+    en:
+      "A computer once said in his language : " +
+      "I write a Diary but tears the last two pages, I eat Almonds but spits the first two",
+    ar: "----",
+  },
   "1_hearts": { en: "I can be broken, but not repaired with glue", ar: "----" },
   "2_hearts": { en: "----", ar: "زوج من ." },
-  "3_hearts": { en: "The Love Triangle", ar: "----" },
+  "3_hearts": { en: "Love Triangle", ar: "----" },
   "4_hearts": { en: "All year seasons have emotions", ar: "----" },
   "5_hearts": {
     en: "English vowels have feelings too!",
@@ -85,7 +100,10 @@ const CardsHints = {
     en: "Lets add Pluto to the group and assign the four chambers to their tasks",
     ar: "----",
   },
-  "10_hearts": { en: "----", ar: "----" },
+  "10_hearts": {
+    en: "I'm blood red but also the start of a table, middle of a leg and the end of fun",
+    ar: "----",
+  },
   king_clubs: { en: "----", ar: "----" },
   king_spades: {
     en: "Can Alice defeat the Undefeatable soldier ?",
@@ -94,7 +112,7 @@ const CardsHints = {
   king_diamonds: { en: "----", ar: "----" },
   king_hearts: { en: "----", ar: "----" },
   jack_clubs: { en: "----", ar: "----" },
-  jack_spades: { en: "The prince is in the Army !", ar: "الأمير في الجيش !" },
+  jack_spades: { en: "The prince is in the Army !", ar: "! الأمير في الجيش" },
   jack_diamonds: { en: "----", ar: "----" },
   jack_hearts: {
     en: "Alice can not see her own mark but she can see other's marks, she needs to trust at least one person",
